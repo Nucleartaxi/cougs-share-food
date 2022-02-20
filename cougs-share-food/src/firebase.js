939cf15a162bf8a495e9774952b1335b16dc4da5
nicholaskraabel <71398709+nrkraabel@users.noreply.github.com>
2022-02-19 21:02:38 -0800

    finished uploading images to firebase

---
+++ cougs-share-food/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+var firebaseConfig = {
+  apiKey: "AIzaSyDrffMVjWpzoR-11bVwcexhI07XcwuJXag",
+  authDomain: "cougssharefood.firebaseapp.com",
+  projectId: "cougssharefood",
+  storageBucket: "cougssharefood.appspot.com",
+  messagingSenderId: "371170372090",
+  appId: "1:371170372090:web:639d2ac01e9db508869c43",
+};
+
+firebase.initializeApp(firebaseConfig);
+const storage = firebase.storage();
+
+export default storage;
--- cougs-share-food/src/pages/inputForm.js
+++ cougs-share-food/src/pages/inputForm.js
@@ -1,58 +1,120 @@
-import React,{useState} from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import storage from "../firebase";
 
 class post {
-  constructor(foodName, pickupLocation, contact, imagePath) {
+  constructor(foodName, pickupLocation, contact, ImagePath) {
     this.foodName = foodName;
     this.pickupLocation = pickupLocation;
     this.contact = contact;
-    this.imagePath = imagePath;
+    this.imagePath = ImagePath;
   }
 }
+
 export default function InputForm() {
+  const allInputs = { imgUrl: "" };
+  const [imageAsFile, setImageAsFile] = useState("");
+  const [imageAsUrl, setImageAsUrl] = useState(allInputs);
   const [foodName, setFoodName] = useState("");
   const [pickupLocation, setPickupLocation] = useState("");
   const [contact, setContactLocation] = useState("");
+  const [file, setFile] = useState(null);
   const posts = [];
-
 
   const fFoodName = (evt) => {
     setFoodName(evt.target.value);
-  }
+  };
   const fPickupLocation = (evt) => {
     setPickupLocation(evt.target.value);
-  }
+  };
   const fContact = (evt) => {
     setContactLocation(evt.target.value);
-  }
+  };
+
+  console.log(imageAsFile);
+  const handleImageAsFile = (e) => {
+    const image = e.target.files[0];
+    setImageAsFile((imageFile) => image);
+  };
+
   const submitForm = (evt) => {
-    const o = new post(foodName, pickupLocation, contact);
+    evt.preventDefault();
+    console.log("start of upload");
+    if (imageAsFile === "") {
+      console.error(`not an image, the image file is a ${typeof imageAsFile}`);
+    }
+
+    const uploadTask = storage
+      .ref(`/images/${imageAsFile.name}`)
+      .put(imageAsFile);
+    //initiates the firebase side uploading
+    uploadTask.on(
+      "state_changed",
+      (snapShot) => {
+        //takes a snap shot of the process as it is happening
+        console.log(snapShot);
+      },
+      (err) => {
+        //catches the errors
+        console.log(err);
+      },
+      () => {
+        // gets the functions from storage refences the image storage in firebase by the children
+        // gets the download url then sets the image from firebase as the value for the imgUrl key:
+        storage
+          .ref("images")
+          .child(imageAsFile.name)
+          .getDownloadURL()
+          .then((fireBaseUrl) => {
+            setImageAsUrl((prevObject) => ({
+              ...prevObject,
+              imgUrl: fireBaseUrl,
+            }));
+          });
+      }
+    );
+    const o = new post(foodName, pickupLocation, contact, file);
     posts.push(o);
-    console.log(o);
-    // console.log(foodName);
-    // console.log(pickupLocation);
-    // console.log(contact);
-    console.log(posts);
-  }
+  };
   return (
     <div>
-      <p>test text</p>
+      <p className="test">test text</p>
       <Box
         component="form"
         sx={{
-          '& > :not(style)': { m: 1, width: '25ch' },
+          "& > :not(style)": { m: 1, width: "25ch" },
         }}
         noValidate
         autoComplete="off"
       >
-        <TextField id="outlined-basic" label="Food Name" variant="outlined" onChange={fFoodName} value={foodName} />
-        <TextField id="outlined-basic" label="Pickup Location" variant="outlined" onChange={fPickupLocation} value={pickupLocation} />
-        <TextField id="outlined-basic" label="Contact" variant="outlined" onChange={fContact} value={contact} />
+        <TextField
+          id="outlined-basic"
+          label="Food Name"
+          variant="outlined"
+          onChange={fFoodName}
+          value={foodName}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Pickup Location"
+          variant="outlined"
+          onChange={fPickupLocation}
+          value={pickupLocation}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Contact"
+          variant="outlined"
+          onChange={fContact}
+          value={contact}
+        />
       </Box>
-      <Button onClick={submitForm} variant="contained">test</Button>
+      <input type="file" onChange={handleImageAsFile} />
+      <Button onClick={submitForm} variant="contained">
+        Submit
+      </Button>
     </div>
   );
 }
@@ -60,4 +122,3 @@ export default function InputForm() {
 // function inputForm() {
 //   return <div></div>;
 // }
-
