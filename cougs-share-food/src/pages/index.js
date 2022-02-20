65ec289fb98d58b471d2e7d2363c99f512ad3407
Alexander Shirk <alexander@truewheels.net>
2022-02-19 16:46:28 -0800

    displays in line but has no spacing

--- cougs-share-food/src/pages/index.js
+++ cougs-share-food/src/pages/index.js
@@ -1,11 +1,16 @@
 import React from "react";
+import "./homepage.css";
 
 function handleCLick() {}
 
 function Home() {
   return (
-    <div>
-      <h1>Coug Share Food</h1>
+    <div className="page">
+      <div className="topBar">
+        <p className="search">Search</p>
+        <p className="title">Cougs Share Food</p>
+        <p className="new">new</p>
+      </div> 
 
       <button onClick={handleCLick}> add</button>
     </div>
