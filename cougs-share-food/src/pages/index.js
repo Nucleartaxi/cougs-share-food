8257ad634c949331cc6b41f054006326346b3849
Alexander Shirk <alexander@truewheels.net>
2022-02-19 17:26:52 -0800

    wip

--- cougs-share-food/src/pages/index.js
+++ cougs-share-food/src/pages/index.js
@@ -5,14 +5,23 @@ function handleCLick() {}
 
 function Home() {
   return (
-    <div className="page">
-      <div className="topBar">
-        <p className="search">Search</p>
-        <p className="title">Cougs Share Food</p>
-        <p className="new">new</p>
-      </div> 
+    <div>
+      {/* <table width="100%" className="myTable">
+        <tr>
+          <th>search</th>
+          <th>Cougs Share Food</th>
+          <th>new</th>
+        </tr>
+      </table> */}
+      <div className="page"> {/* <div className="topBar">
+          <p className="search">Search</p>
+          <p className="title">Cougs Share Food</p>
+          <p className="new">new</p>
+        </div>  */}
 
-      <button onClick={handleCLick}> add</button>
+        <button onClick={handleCLick}> add</button>
+      </div>
+
     </div>
   );
 }
